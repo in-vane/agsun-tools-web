8aa1de62c78178a1a29428cc4e6ff63048d82380
in_vane <in_vane@192.168.5.10>
2024-11-07 10:23:32 +0800

    feat(file): add notification when uploaded files are same

--- src/store/modules/file.js
+++ src/store/modules/file.js
@@ -19,11 +19,21 @@ export const useFileStore = defineStore({
   actions: {
     async updateFiles(fileList) {
       this.currentFiles = [];
+      const infoList = [];
       for (let item of fileList) {
         const file = item.file;
         const md5 = await getMD5(file);
         item.md5 = md5;
-        const isCurHas = this.currentFiles.some((_) => item.md5 == _.md5);
+        // 前台文件查重
+        const isCurHas = this.currentFiles.some((_) => {
+          const isSame = item.md5 == _.md5;
+          if (isSame) {
+            infoList[0] = item.name;
+            infoList[1] = _.name;
+          }
+          return isSame;
+        });
+        // 历史文件查重
         const isHisHas = this.historyFiles.some((_) => item.md5 == _.md5);
 
         if (!isCurHas) {
@@ -35,7 +45,14 @@ export const useFileStore = defineStore({
         if (this.historyFiles.length > 5) {
           this.historyFiles.shift();
         }
-        console.log(this.historyFiles)
+        console.log(this.historyFiles);
+      }
+      if (infoList.length) {
+        const $notification = window['$notification'];
+        $notification.info({
+          title: '前台文件相同',
+          content: `文件列表中存在相同文件，它们的文件名可能不同，请检查。`,
+        });
       }
     },
     clearCurrentFiles() {
